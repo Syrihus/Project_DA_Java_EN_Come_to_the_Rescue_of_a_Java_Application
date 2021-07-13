36efa04e761a718777d458bdd5beda873b9d4647
Arnaud <arnaud.dev.java@gmail.com>
2021-07-13 15:20:44 +0200

    edit AnalyticsCounter class to have a static void main compact. Edit file path and add comments

--- Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
@@ -1,43 +1,65 @@
 package com.hemebiotech.analytics;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.FileWriter;
-
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
+import java.io.IOException;
+/**
+ * This class call methods to have a compact static void main
+ * @author Arnaud
+ *
+ */
 public class AnalyticsCounter {
-	private static int headacheCount = 0;	// initialize to 0
-	private static int rashCount = 0;		// initialize to 0
-	private static int pupilCount = 0;		// initialize to 0
-	
-	public static void main(String args[]) throws Exception {
-		// first get input
-		BufferedReader reader = new BufferedReader (new FileReader("symptoms.txt"));
-		String line = reader.readLine();
 
-		int i = 0;	// set i to 0
-		int headCount = 0;	// counts headaches
-		while (line != null) {
-			i++;	// increment i
-			System.out.println("symptom from file: " + line);
-			if (line.equals("headache")) {
-				headCount++;
-				System.out.println("number of headaches: " + headCount);
-			}
-			else if (line.equals("rush")) {
-				rashCount++;
-			}
-			else if (line.contains("pupils")) {
-				pupilCount++;
-			}
-
-			line = reader.readLine();	// get another symptom
-		}
+	public static void main(String args[]) throws IOException{
 		
-		// next generate output
-		FileWriter writer = new FileWriter ("result.out");
-		writer.write("headache: " + headacheCount + "\n");
-		writer.write("rash: " + rashCount + "\n");
-		writer.write("dialated pupils: " + pupilCount + "\n");
-		writer.close();
+		AnalyticsCounter analyticsCounter = new AnalyticsCounter(); 
+
+		ISymptomReader symptomReader = new ReadSymptomDataFromFile("Project02Eclipse/symptoms.txt");
+
+		List<String> symptomListFromFile;
+		try{
+			symptomListFromFile = symptomReader.getSymptoms();
+		} catch (Exception e) {
+			e.printStackTrace();
+			return;
+		}
+
+		Map<String, Integer> symptomMapFrequencies;
+		try{
+			symptomMapFrequencies = analyticsCounter.countSymptomFrequency(symptomListFromFile);
+		} catch (Exception e) {
+			e.printStackTrace();
+			return;
+		}
+	
+	
+		ISymptomWriter symptomWriter = new WriteSymptomDataToFile("Project02Eclipse/results.out");
+		try{
+			symptomWriter.writeSymptomDataToFile(symptomMapFrequencies);
+		}	
+		catch(IOException e){
+			e.printStackTrace();
+			return;
+		}
+	}
+	/**
+	 * cross the symptom list "symptomListFromFile" and count the frequencies and write data in Map symptomMapFrequencies. 
+	 * 
+	 * @param symptomListFromFile
+	 * @return symptomMapFrequencies
+	 * 
+	 */
+	public TreeMap<String, Integer> countSymptomFrequency(List<String> symptomListFromFile) {
+
+		TreeMap<String, Integer> processMap = new TreeMap<String, Integer>(); 
+		
+		if(symptomListFromFile != null) {
+			for(String symptom : symptomListFromFile) {
+				processMap.put(symptom, Collections.frequency(symptomListFromFile, symptom));
+			}
+		}
+		return processMap;	
 	}
 }
